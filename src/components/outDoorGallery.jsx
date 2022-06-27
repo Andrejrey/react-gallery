82b442e37b76fa8e1b0fbeb9257eff753489f87d
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-06-27 17:39:54 +0200

    bugfix image slider navigation

--- src/components/outDoorGallery.jsx
+++ src/components/outDoorGallery.jsx
@@ -1,77 +1,83 @@
-import { MdClose, MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
-import React, { useState } from "react";
+import {MdClose, MdArrowBackIosNew, MdArrowForwardIos} from "react-icons/md";
+import React, {useState} from "react";
 import OutImageData from "../outImageData";
 import TitleBar from "./titleBar";
 import "../gallery.css";
 import AppPagination from "./AppPagination";
 
 const OutDoorGallery = (imgSrc) => {
-  const [image, setImage] = useState(false);
-  const [tempImgSrc, setTempImgSrc] = useState("");
-  const [currentImage, setCurrentImage] = useState(1);
-  const length = OutImageData.length;
+	const [image, setImage] = useState(false);
+	const [tempImg, setTempImg] = useState("");
+	const [currentImageId, setCurrentImageId] = useState(1);
+	const length = OutImageData.length;
 
-  const getImg = (imgSrc) => {
-    setTempImgSrc(imgSrc);
-    setImage(true);
-  };
+	const getImg = (img) => {
+		setCurrentImageId(img.id)
+		setTempImg(img);
+		setImage(true);
+	};
 
-  const nextImage = () => {
-    setCurrentImage(currentImage === length ? 1 : currentImage + 1);
-    const currentSrc = findImageById(currentImage);
+	const nextImage = () => {
+		const id = currentImageId + 1;
+		setCurrentImageId(currentImageId === length ? 1 : id);
+		const current = findImageById(id);
 
-    if (currentSrc) {
-      setTempImgSrc(currentSrc);
-      console.log(currentSrc);
-    }
-  };
+		if (current) {
+			setTempImg(current);
+			console.log("next", currentImageId);
+		}
+	};
 
-  const prevImage = () => {
-    setCurrentImage(currentImage === 1 ? length : currentImage - 1);
-    const currentSrc = findImageById(currentImage);
-    console.log(currentImage);
-    if (currentSrc) {
-      setTempImgSrc(currentSrc);
-    }
-  };
+	const prevImage = () => {
+		const id = currentImageId - 1;
+		setCurrentImageId(currentImageId === 1 ? length : id);
+		const current = findImageById(id);
 
-  const findImageById = (id) => {
-    const img = OutImageData.find((img) => img.id === id);
-    if (img) {
-      return img.imgSrc;
-    }
-    return false;
-  };
+		if (current) {
+			setTempImg(current);
+			console.log("prev", currentImageId);
+		}
+	};
 
-  return (
-    <>
-      <TitleBar />
-      <div className="gallery">
-        <div className="allimg">
-          {OutImageData.map((item, index) => {
-            return (
-              <div
-                className="img"
-                key={index}
-                onClick={() => getImg(item.imgSrc)}
-              >
-                <img src={item.imgSrc} />
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className={image ? "image open" : "image"}>
-        <MdArrowBackIosNew className="back" onClick={prevImage} />
-        <img src={tempImgSrc} />
-        <MdClose className="close" onClick={() => setImage(false)} />
-        <MdArrowForwardIos className="next" onClick={nextImage} />
-      </div>
-      <div className="pagi">
-        <AppPagination />
-      </div>
-    </>
-  );
+	const findImageById = (id) => {
+		return OutImageData.find((img) => img.id === id);
+	};
+
+	let btnPrev = (currentImageId > 1)  ? <MdArrowBackIosNew className="back" onClick={prevImage} /> : '',
+		btnNext = (currentImageId < length) ? <MdArrowForwardIos className="next" onClick={nextImage} /> : '';
+
+	return (
+		<>
+			<TitleBar/>
+			<div className="gallery">
+				<div className="allimg">
+					{
+						OutImageData.map(item => {
+							return (
+								<div
+									className="img"
+									key={item.id}
+									onClick={() => getImg(item)}
+								>
+									<img src={item.imgSrc}/>
+									<div className="info">{item.id}</div>
+								</div>
+							);
+						})
+					}
+				</div>
+			</div>
+			<div className={image ? "image open" : "image"}>
+				{ btnPrev }
+				<img src={tempImg.imgSrc} title={currentImageId} alt={currentImageId} />
+				<MdClose className="close" onClick={() => setImage(false)} />
+				{ btnNext }
+			</div>
+			<div className="pagi">
+				<AppPagination/>
+			</div>
+		</>
+	);
 };
 
 export default OutDoorGallery;
