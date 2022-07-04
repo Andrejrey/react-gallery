0eca773b2ccda3eb60f920e8dd48153aaedf8629
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-07-04 15:23:54 +0200

    add axios functionality

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -1,64 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import TitleBar from "./titleBar";
 
 import "../aboutMe.css";
 
 const AboutMe = () => {
+  const [aboutMe, setAboutMe] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/about_me")
+      .then(function (response) {
+        // handle success
+        console.log("Axios response", response);
+        setAboutMe(response.data);
+        console.log(aboutMe);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
+
   return (
     <div className="bio-body">
       <TitleBar />
       <div className="biography">
         <div className="biography-box">
-          <div className="bio-right">
-            <h2>Biography</h2>
-            <text>
-              Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam natus omnis sunt dignissimos soluta numquam! Aperiam doloremque, vero
-              voluptates saepe quibusdam labore, ratione explicabo accusamus ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Magni temporibus nam natus omnis sunt dignissimos soluta numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore, ratione
-              explicabo accusamus ipsa id minima.
-            </text>
-            <text className="text2">
-              Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam natus omnis sunt dignissimos soluta numquam! Aperiam doloremque, vero
-              voluptates saepe quibusdam labore, ratione explicabo accusamus ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Magni temporibus nam natus omnis sunt dignissimos soluta numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore, ratione
-              explicabo accusamus ipsa id minima.
-            </text>
+          <div className="bio-left">
+            <h2>{aboutMe.title}</h2>
+            <div>{aboutMe.text}</div>
           </div>
-          <div className="bio-left"></div>
+          <div className="bio-right">
+            <img src={aboutMe.imgSrc} alt="" />
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default AboutMe;
-
-/* <div className="titlebar">
-<TitleBar />
-  </div>
-  <div className="aboutme">
-    <div className="biography">
-      <h1>Biography</h1>
-    </div>
-    <div className="text">
-      <h3>Lorem ipsum </h3>
-      <text>
-        Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam
-        natus omnis sunt dignissimos soluta numquam! Aperiam doloremque,
-        vero voluptates saepe quibusdam labore, ratione explicabo accusamus
-        ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magni temporibus nam natus omnis sunt dignissimos soluta
-        numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore,
-        ratione explicabo accusamus ipsa id minima.
-      </text>
-      <text className="text2">
-        Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam
-        natus omnis sunt dignissimos soluta numquam! Aperiam doloremque,
-        vero voluptates saepe quibusdam labore, ratione explicabo accusamus
-        ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magni temporibus nam natus omnis sunt dignissimos soluta
-        numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore,
-        ratione explicabo accusamus ipsa id minima.
-      </text>
-    </div>
-    <img src="/bio.jpg" className="bioimg" alt="" />
-  </div> */
--- src/components/inDoorGallery.jsx
+++ src/components/inDoorGallery.jsx
@@ -1,69 +1,92 @@
 import { MdClose, MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../gallery.css";
-import InImageData from "../inImagedata";
 import TitleBar from "./titleBar";
 import AppPagination from "./AppPagination";
 
 const InDoorGallery = () => {
   const [image, setImage] = useState(false);
-  const [tempImgSrc, setTempImgSrc] = useState("");
-  const [currentImage, setCurrentImage] = useState(1);
-  const length = InImageData.length;
+  const [tempImg, setTempImg] = useState(null);
+  const [currentImageId, setCurrentImageId] = useState(1);
+  const [length, setLength] = useState();
+  const [inImageData, setOutImagedata] = useState([]);
 
-  const getImg = (imgSrc) => {
-    setTempImgSrc(imgSrc);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/indoor")
+      .then(function (response) {
+        // handle success
+        console.log("Axios response", response);
+        setOutImagedata(response.data);
+        setLength(response.data.length);
+        console.log(inImageData);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
+
+  const getImg = (img) => {
+    setCurrentImageId(img.id);
+    setTempImg(img);
     setImage(true);
   };
 
   const nextImage = () => {
-    setCurrentImage(currentImage === length ? 1 : currentImage + 1);
-    const currentSrc = findImageById(currentImage);
-    if (currentSrc) {
-      setTempImgSrc(currentSrc);
+    const id = currentImageId + 1;
+    setCurrentImageId(currentImageId === length ? 1 : id);
+    const current = findImageById(id);
+
+    if (current) {
+      setTempImg(current);
+      console.log("next", currentImageId);
     }
   };
 
   const prevImage = () => {
-    setCurrentImage(currentImage === 1 ? length : currentImage - 1);
-    const currentSrc = findImageById(currentImage);
+    const id = currentImageId - 1;
+    setCurrentImageId(currentImageId === 1 ? length : id);
+    const current = findImageById(id);
 
-    if (currentSrc) {
-      setTempImgSrc(currentSrc);
+    if (current) {
+      setTempImg(current);
+      console.log("prev", currentImageId);
     }
   };
 
   const findImageById = (id) => {
-    const img = InImageData.find((img) => img.id === id);
-    if (img) {
-      return img.imgSrc;
-    }
-    return false;
+    return inImageData.find((img) => img.id === id);
   };
+
+  let btnPrev = currentImageId > 1 ? <MdArrowBackIosNew className="back" onClick={prevImage} /> : "",
+    btnNext = currentImageId < length ? <MdArrowForwardIos className="next" onClick={nextImage} /> : "",
+    showImg = tempImg ? <img src={tempImg.imgSrc} title={currentImageId} alt={currentImageId} /> : "";
 
   return (
     <>
       <TitleBar />
       <div className="gallery">
         <div className="allimg">
-          {InImageData.map((item, index) => {
+          {inImageData.map((item) => {
             return (
-              <div
-                className="img"
-                key={index}
-                onClick={() => getImg(item.imgSrc)}
-              >
-                <img src={item.imgSrc} />
+              <div className="img" key={item.id} onClick={() => getImg(item)}>
+                <img src={item.imgSrc} title={currentImageId} alt={currentImageId} />
+                <div className="info">{item.id}</div>
               </div>
             );
           })}
-          <div className={image ? "image open" : "image"}>
-            <MdArrowBackIosNew className="back" onClick={prevImage} />
-            <img src={tempImgSrc} />
-            <MdClose className="close" onClick={() => setImage(false)} />
-            <MdArrowForwardIos className="next" onClick={nextImage} />
-          </div>
         </div>
+      </div>
+      <div className={image ? "image open" : "image"}>
+        {btnPrev}
+        {showImg}
+        <MdClose className="close" onClick={() => setImage(false)} />
+        {btnNext}
       </div>
       <div className="pagi">
         <AppPagination />
