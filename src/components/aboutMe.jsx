5cab0daff38a0a0b731fe2cf3a98dda41505426b
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-07-04 10:45:10 +0200

    add new style for about me and contact

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -1,45 +1,64 @@
-import { MdOutlineKeyboardBackspace } from "react-icons/md";
-import { Link } from "react-router-dom";
+import TitleBar from "./titleBar";
+
 import "../aboutMe.css";
 
 const AboutMe = () => {
   return (
-    <>
-      <div className="titlebar">
-        {/*<a>Outdoor Images</a> */}
-        <Link to="/">
-          <MdOutlineKeyboardBackspace />
-        </Link>
-      </div>
-      <div className="aboutme">
-        <div className="biography">
-          <h1>Biography</h1>
+    <div className="bio-body">
+      <TitleBar />
+      <div className="biography">
+        <div className="biography-box">
+          <div className="bio-right">
+            <h2>Biography</h2>
+            <text>
+              Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam natus omnis sunt dignissimos soluta numquam! Aperiam doloremque, vero
+              voluptates saepe quibusdam labore, ratione explicabo accusamus ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Magni temporibus nam natus omnis sunt dignissimos soluta numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore, ratione
+              explicabo accusamus ipsa id minima.
+            </text>
+            <text className="text2">
+              Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam natus omnis sunt dignissimos soluta numquam! Aperiam doloremque, vero
+              voluptates saepe quibusdam labore, ratione explicabo accusamus ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Magni temporibus nam natus omnis sunt dignissimos soluta numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore, ratione
+              explicabo accusamus ipsa id minima.
+            </text>
+          </div>
+          <div className="bio-left"></div>
         </div>
-        <div className="text">
-          <h3>Lorem ipsum </h3>
-          <text>
-            Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam
-            natus omnis sunt dignissimos soluta numquam! Aperiam doloremque,
-            vero voluptates saepe quibusdam labore, ratione explicabo accusamus
-            ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Magni temporibus nam natus omnis sunt dignissimos soluta
-            numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore,
-            ratione explicabo accusamus ipsa id minima.
-          </text>
-          <text className="text2">
-            Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam
-            natus omnis sunt dignissimos soluta numquam! Aperiam doloremque,
-            vero voluptates saepe quibusdam labore, ratione explicabo accusamus
-            ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Magni temporibus nam natus omnis sunt dignissimos soluta
-            numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore,
-            ratione explicabo accusamus ipsa id minima.
-          </text>
-        </div>
-        <img src="/bio.jpg" className="bioimg" alt="" />
       </div>
-    </>
+    </div>
   );
 };
 
 export default AboutMe;
+
+/* <div className="titlebar">
+<TitleBar />
+  </div>
+  <div className="aboutme">
+    <div className="biography">
+      <h1>Biography</h1>
+    </div>
+    <div className="text">
+      <h3>Lorem ipsum </h3>
+      <text>
+        Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam
+        natus omnis sunt dignissimos soluta numquam! Aperiam doloremque,
+        vero voluptates saepe quibusdam labore, ratione explicabo accusamus
+        ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Magni temporibus nam natus omnis sunt dignissimos soluta
+        numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore,
+        ratione explicabo accusamus ipsa id minima.
+      </text>
+      <text className="text2">
+        Dolor sit, amet consectetur adipisicing elit. Magni temporibus nam
+        natus omnis sunt dignissimos soluta numquam! Aperiam doloremque,
+        vero voluptates saepe quibusdam labore, ratione explicabo accusamus
+        ipsa id minima. Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Magni temporibus nam natus omnis sunt dignissimos soluta
+        numquam! Aperiam doloremque, vero voluptates saepe quibusdam labore,
+        ratione explicabo accusamus ipsa id minima.
+      </text>
+    </div>
+    <img src="/bio.jpg" className="bioimg" alt="" />
+  </div> */
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,25 +3,23 @@ import TitleBar from "./titleBar";
 
 const Contact = () => {
   return (
-    <div className="body">
+    <>
       <TitleBar />
-      <div className="contact">
-        <div className="contact-box">
-          <div className="left"></div>
-          <div className="right">
-            <h2>Contact Us</h2>
-            <input
-              type="email"
-              className="field"
-              placeholder="Your E-mail"
-            ></input>
-            <input type="text" className="field" placeholder="Subject"></input>
-            <textarea className="field area" placeholder="Message"></textarea>
-            <button className="btn">Send</button>
+      <div className="contact-body">
+        <div className="contact">
+          <div className="contact-box">
+            <div className="left"></div>
+            <div className="right">
+              <h2>Contact Us</h2>
+              <input type="email" className="field" placeholder="Your E-mail"></input>
+              <input type="text" className="field" placeholder="Subject"></input>
+              <textarea className="field area" placeholder="Message"></textarea>
+              <button className="btn">Send</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
